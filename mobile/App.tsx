b1bb1062301c1b429e1f33fc99f404581743f82b
wnddddw <911852755@qq.com>
2026-06-19 13:42:06 +0800

    feat: 移动端从 WebView 壳切换为纯原生导航 (RootNavigator)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import WebApp from './src/WebApp';
+import { AuthProvider } from './src/store/AuthContext';
+import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
-  return <WebApp />;
+  return (
+    <AuthProvider>
+      <RootNavigator />
+    </AuthProvider>
+  );
 }
